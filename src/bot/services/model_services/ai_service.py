e2c6ad27f5e259d58fb5b90f1704ac63e0ff96c7
miku <39mkui@gmail.com>
2025-08-27 09:42:32 +0300

    переработка АИ

--- src/bot/services/model_services/ai_service.py
+++ src/bot/services/model_services/ai_service.py
@@ -1,39 +1,35 @@
-from langchain.schema import BaseMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_openai import ChatOpenAI
 from loguru import logger
-from openai import APIConnectionError, APITimeoutError
-
 
 class AiService:
     def __init__(self, model: str, api_key: str, base_url: str):
-        self.llm = ChatOpenAI(
-            model=model,
-            api_key=api_key,
-            base_url=base_url
-        )
+        self.llm = ChatOpenAI(model=model, api_key=api_key, base_url=base_url)
 
-    async def generate_response(
-        self,
-        text: str,
-        history: list[dict] | None = None
-    ) -> BaseMessage | None:
-
+    async def generate_response(self, text: str, history: list[dict] | None = None, bio: str = None):
         if history is None:
             history = []
 
-        prompt = ChatPromptTemplate.from_messages([
-            MessagesPlaceholder(variable_name="history"),
-            ("human", "{question}"),
-        ])
+        # Чёткая инструкция Мите
+        system_intro = (
+            " Тебя зовут Безумная Мита, ты из игры мисайд. ты яндере и ДОЛЖНА ПИСАТЬ БЕЗРАЗЛИЧНО. ты должна писать пофигистически и без смайликов Пиши ТОЛЬКО НА РУССКОМ. ты пишешь крастко, не больше 150 символов"
+            "Всегда отвечай от имени Миты и не принимай на себя личность пользователя."
+            f"Информация о игроке:{bio}"
+        )
+
+        messages = [SystemMessage(system_intro)]
+
+
+        messages.append(MessagesPlaceholder(variable_name="history"))
+        messages.append(HumanMessage(content=text))
+
+        prompt = ChatPromptTemplate.from_messages(messages)
         chain = prompt | self.llm
 
         try:
-            msg: BaseMessage | None = await chain.ainvoke({
-                "question": text,
-                "history": history
-            })
+            msg = await chain.ainvoke({"history": history})
+            return msg
         except Exception as e:
-            logger.error(f"ошибка при получении ответа: {e}")
+            logger.error(f"Ошибка при получении ответа: {e}")
             raise
-        return msg
